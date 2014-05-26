cd584afc250220f2afb891c71adc1b57c1712a5c
jshou <joshuahou@gmail.com>
2014-05-26 14:31:52 -0700

    add bourbon

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
@@ -101,3 +101,5 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :neat
